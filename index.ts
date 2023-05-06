5eb3f219059b485e5e5e998d7d081965608c0b83
Bobby Donchev <contact@donchev.is>
2023-05-06 10:29:55 +0200

    export types to library users

--- index.ts
+++ index.ts
@@ -1,1 +1,6 @@
-export { default as GetTestMail } from './lib/client';
+export { default as GetTestMail } from './lib/client'
+export { GetTestMail as Mailbox } from './lib/models'
+export { APIError } from './lib/models'
+export { Problem } from './lib/models'
+export { Message } from './lib/models'
+export { Attachment } from './lib/models'
